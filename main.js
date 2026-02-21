4ac8ec2dc0ead15a681c98a6e4dda772890e2e44
Robin Zollner <code@bravegrumpy.com>
2026-02-21 17:32:44 -0500

    Adding a model that pops up when buttons are clicked.

---
+++ main.js
@@ -0,0 +1,19 @@
+function showModal() {
+    const modal = document.getElementById("modal");
+    modal.style = undefined;
+    const dialog = document.querySelector("dialog");
+    dialog.open = true;
+    document.documentElement.style.overflow = "hidden";
+    const main = document.getElementById("main");
+    main.style.display = "none";
+}
+
+function hideModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+    const dialog = document.querySelector("dialog");
+    dialog.open = false;
+    document.documentElement.style = undefined;
+    const main = document.getElementById("main");
+    main.style = undefined;
+}
